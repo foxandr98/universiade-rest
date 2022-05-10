de946c5490e6ef2d0a32d2aa4f624589b83d53ce
Alex Fox <foxandr98@mail.ru>
2022-05-10 23:44:49 +0500

    Initialization

---
+++ src/main/java/net/foxandr/sport/universiade/UniversiadeRestApiApplication.java
@@ -0,0 +1,13 @@
+package net.foxandr.sport.universiade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversiadeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversiadeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/foxandr/sport/universiade/UniversiadeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.foxandr.sport.universiade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversiadeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
